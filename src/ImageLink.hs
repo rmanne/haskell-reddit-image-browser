ff55c7f88ec29c6f7edc1d4ae953b8d1efb149ae
Rahul Manne <rahul_manne@hotmail.com>
2022-09-25 16:10:32 -0700

    Use youtube-dl for v.redd.it, gfycat, redgifs

--- src/ImageLink.hs
+++ src/ImageLink.hs
@@ -1,186 +1,203 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module ImageLink
-  ( imageLinks
-  ) where
+  ( imageLinks,
+    DownloadHandler (..),
+  )
+where
 
-import Control.Exception (IOException, catch, throw)
+import Control.Exception (catch)
 import Control.Lens ((^.))
 import Control.Monad (foldM)
+import Data.Aeson (FromJSON (parseJSON), Object, Value (Array, Object), (.:))
 import qualified Data.Aeson as Aeson
-import Data.Aeson (FromJSON(parseJSON), Object, Value(Array, Object), (.:))
+import qualified Data.Aeson.Key as Key
+import qualified Data.Aeson.KeyMap as HashMap
 import Data.Aeson.Types (typeMismatch)
 import Data.Char (isSpace)
-import qualified Data.Aeson.KeyMap as HashMap
 import Data.List (dropWhileEnd, isSuffixOf)
-import qualified Data.Aeson.Key as Key
 import qualified Data.Vector as Vector
-import Network.HTTP.Client
-  ( HttpException(HttpExceptionRequest)
-  , HttpExceptionContent(StatusCodeException)
-  )
+import Network.HTTP.Client (HttpException (HttpExceptionRequest))
 import qualified Network.Wreq as Wreq
 import System.Process (readCreateProcess, shell)
 import Text.Regex.PCRE ((=~))
 
-newtype Gfycat =
-  Gfycat
-    { mp4Url :: String
-    }
-  deriving (Show)
-
-instance FromJSON Gfycat where
-  parseJSON (Object v) = v .: "gfyItem" >>= \o -> Gfycat <$> o .: "mp4Url"
-  parseJSON v = typeMismatch "Gfycat response should be an aeson object" v
-
-newtype RedditGallery =
-  RedditGallery
-    { redditUrls :: [String]
-    }
+newtype RedditGallery = RedditGallery
+  { redditUrls :: [String]
+  }
   deriving (Show)
 
 instance FromJSON RedditGallery where
   parseJSON (Array v) =
-    (case Vector.head v of
-       Object o -> o .: "data"
-       _ -> typeMismatch "Response should be an array of objects" (Array v)) >>= \d ->
-      d .: "children" >>= \[c] ->
-        c .: "data" >>= \d' ->
-          d' .: "media_metadata" >>= \(mediaMetadata :: Object) ->
-            mapM
-              (\(mediaId, Object itemDetails) ->
-                 itemDetails .: "m" >>= \(itemType :: String) ->
-                   case itemType of
-                     "image/jpg" ->
-                       return
-                         ["https://i.redd.it/" ++ Key.toString mediaId ++ ".jpg"]
-                     "image/png" ->
-                       return
-                         ["https://i.redd.it/" ++ Key.toString mediaId ++ ".png"]
-                     "image/gif" ->
-                       return
-                         ["https://i.redd.it/" ++ Key.toString mediaId ++ ".gif"]
-                     _ -> return [])
-              (HashMap.toList mediaMetadata) >>= \urls ->
-              return (RedditGallery (concat urls))
+    ( case Vector.head v of
+        Object o -> o .: "data"
+        _ -> typeMismatch "Response should be an array of objects" (Array v)
+    )
+      >>= \d ->
+        d .: "children" >>= \[c] ->
+          c .: "data" >>= \d' ->
+            d' .: "media_metadata" >>= \(mediaMetadata :: Object) ->
+              mapM
+                ( \(mediaId, Object itemDetails) ->
+                    itemDetails .: "m" >>= \(itemType :: String) ->
+                      case itemType of
+                        "image/jpg" ->
+                          return
+                            ["https://i.redd.it/" ++ Key.toString mediaId ++ ".jpg"]
+                        "image/png" ->
+                          return
+                            ["https://i.redd.it/" ++ Key.toString mediaId ++ ".png"]
+                        "image/gif" ->
+                          return
+                            ["https://i.redd.it/" ++ Key.toString mediaId ++ ".gif"]
+                        _ -> return []
+                )
+                (HashMap.toList mediaMetadata)
+                >>= \urls ->
+                  return (RedditGallery (concat urls))
   parseJSON v = typeMismatch "Response should be an aeson object" v
 
 trim :: String -> String
 trim = dropWhileEnd isSpace . dropWhile isSpace
 
-imageLinks :: String -> IO [String]
+imageLinks :: Handler
 imageLinks link =
   foldM
-    (\links handler ->
-       if null links
-         then filter (not . null) <$>
-              (handler link `catch` \e ->
-                 putStrLn
-                   ("Failed " ++
-                    link ++ "; Caught " ++ show (e :: HttpException)) >>
-                 return [])
-         else return links)
+    ( \links handler ->
+        if null links
+          then
+            filter (not . null)
+              <$> ( handler link `catch` \e ->
+                      putStrLn
+                        ( "Failed "
+                            ++ show link
+                            ++ "; Caught "
+                            ++ show (e :: HttpException)
+                        )
+                        >> return []
+                  )
+          else return links
+    )
     []
     handlers
 
-type Handler = String -> IO [String]
+data DownloadHandler = YoutubeDL | DirectLink deriving (Show)
+
+type Handler = String -> IO [(String, DownloadHandler)]
+
+type DirectLinkHandler = String -> IO [String]
 
 handlers :: [Handler]
 handlers =
-  [ imageHandler
-  , baseImageHandler
-  , gifvHandler
-  , imgurImageHandler
-  , imgurAlbumHandler
-  , imgurGalleryHandler
-  , imgurOtherHandler
-  , gfycatHandler
-  , redgifHandler
-  , redditGalleryHandler
+  [ fmap (map (,DirectLink)) . redditPreviewHandler,
+    fmap (map (,DirectLink)) . imageHandler,
+    fmap (map (,DirectLink)) . baseImageHandler,
+    fmap (map (,DirectLink)) . gifvHandler,
+    fmap (map (,DirectLink)) . imgurImageHandler,
+    fmap (map (,DirectLink)) . imgurAlbumHandler,
+    fmap (map (,DirectLink)) . imgurGalleryHandler,
+    fmap (map (,DirectLink)) . imgurOtherHandler,
+    fmap (map (,YoutubeDL)) . gfycatHandler,
+    fmap (map (,YoutubeDL)) . redgifHandler,
+    fmap (map (,DirectLink)) . redditGalleryHandler,
+    fmap (map (,YoutubeDL)) . redditVideoHandler,
+    fmap (map (,YoutubeDL)) . xvideosHandler
   ]
 
-redditGalleryHandler :: Handler
+redditGalleryHandler :: DirectLinkHandler
 redditGalleryHandler link =
-  case link =~ ("https?://(www.|)reddit.com/gallery/([a-zA-Z0-9]*)" :: String) :: [[String]] of
+  case link =~ ("^https?://(www.|)reddit.com/gallery/([a-zA-Z0-9]*)$" :: String) :: [[String]] of
     [[_, _, redditId]] ->
-      (\case
-         Nothing -> []
-         Just x -> x) .
-      fmap redditUrls . Aeson.decode . (^. Wreq.responseBody) <$>
-      Wreq.get ("https://www.reddit.com/comments/" ++ redditId ++ ".json")
+      ( \case
+          Nothing -> []
+          Just x -> x
+      )
+        . fmap redditUrls
+        . Aeson.decode
+        . (^. Wreq.responseBody)
+        <$> Wreq.get ("https://www.reddit.com/comments/" ++ redditId ++ ".json")
     _ -> return []
 
-imageHandler :: Handler
+imageHandler :: DirectLinkHandler
 imageHandler link =
-  if isSuffixOf ".jpg" link ||
-     isSuffixOf ".jpgtrue" link ||
-     isSuffixOf ".jpeg" link ||
-     isSuffixOf ".png" link ||
-     isSuffixOf ".webp" link ||
-     isSuffixOf ".webm" link || isSuffixOf ".gif" link || isSuffixOf ".mp4" link
+  if isSuffixOf ".jpg" link
+    || isSuffixOf ".jpgtrue" link
+    || isSuffixOf ".jpeg" link
+    || isSuffixOf ".png" link
+    || isSuffixOf ".webp" link
+    || isSuffixOf ".webm" link
+    || isSuffixOf ".gif" link
+    || isSuffixOf ".mp4" link
     then return [link]
     else return []
 
-baseImageHandler :: Handler
+baseImageHandler :: DirectLinkHandler
 baseImageHandler link =
-  case link =~ ("(.*)\\?[^?]*" :: String) :: [[String]] of
+  case link =~ ("^(.*)\\?[^?]*$" :: String) :: [[String]] of
     [[_, baseUrl]] ->
       imageHandler baseUrl >>= \case
         [] -> return []
         _ -> return [link]
     _ -> return []
 
-gifvHandler :: Handler
+gifvHandler :: DirectLinkHandler
 gifvHandler link =
   if ".gifv" `isSuffixOf` link
     then return [take (length link - 4) link ++ "mp4"]
     else return []
 
-imgurImageHandler :: Handler
+imgurImageHandler :: DirectLinkHandler
 imgurImageHandler link = do
-  case link =~ ("https?://(www.)?imgur.com/[a-zA-Z0-9]*" :: String) :: [[String]] of
+  case link =~ ("^https?://(www.)?imgur.com/[a-zA-Z0-9]*$" :: String) :: [[String]] of
     [_] ->
-      (: []) . trim <$>
-      readCreateProcess
-        (shell $
-         "curl -L -s '" ++
-         link ++ "' | sed -n 's/.*image_src.*href=\"\\(.*\\)\".*/\\1/p'")
-        ""
+      (: []) . trim
+        <$> readCreateProcess
+          ( shell $
+              "curl -L -s '"
+                ++ link
+                ++ "' | sed -n 's/.*image_src.*href=\"\\(.*\\)\".*/\\1/p'"
+          )
+          ""
     _ -> return []
 
-imgurAlbumHandler :: Handler
+imgurAlbumHandler :: DirectLinkHandler
 imgurAlbumHandler link =
-  case link =~ ("https?://(www.)?imgur.com/a/[a-zA-Z0-9]*" :: String) :: [[String]] of
+  case link =~ ("^https?://(www.)?imgur.com/a/[a-zA-Z0-9]*$" :: String) :: [[String]] of
     [_] -> do
       putStrLn "Found imgur album, not yet fully implemented"
       link' <-
-        trim <$>
-        readCreateProcess
-          (shell $
-           "curl -L -s '" ++
-           link ++ "' | sed -n 's/.*image_src.*href=\"\\([^\"]*\\)\".*/\\1/p'")
-          ""
+        trim
+          <$> readCreateProcess
+            ( shell $
+                "curl -L -s '"
+                  <> link
+                  <> "' | sed -n -e 's/.*image_src.*href=\"\\([^\"]*\\)\".*/\\1/p' -e 's/.*twitter:player:stream.*content=\"\\([^\"]*\\.mp4\\)\".*/\\1/p'"
+            )
+            ""
       return [link']
     _ -> return []
 
-imgurGalleryHandler :: Handler
+imgurGalleryHandler :: DirectLinkHandler
 imgurGalleryHandler link =
-  case link =~ ("https?://(www.)?imgur.com/gallery/[a-zA-Z0-9]*" :: String) :: [[String]] of
+  case link =~ ("^https?://(www.)?imgur.com/gallery/[a-zA-Z0-9]*$" :: String) :: [[String]] of
     [_] -> do
       putStrLn "Found imgur gallery, not yet fully implemented"
       link' <-
-        trim <$>
-        readCreateProcess
-          (shell $
-           "curl -L -s '" ++
-           link ++ "' | sed -n 's/.*image_src.*href=\"\\(.*\\)\".*/\\1/p'")
-          ""
+        trim
+          <$> readCreateProcess
+            ( shell $
+                "curl -L -s '"
+                  ++ link
+                  ++ "' | sed -n 's/.*image_src.*href=\"\\(.*\\)\".*/\\1/p'"
+            )
+            ""
       return [link']
     _ -> return []
 
-imgurOtherHandler :: Handler
+imgurOtherHandler :: DirectLinkHandler
 imgurOtherHandler link =
-  case link =~ ("https?://(www.|m.)?imgur.com/.*" :: String) :: [[String]] of
+  case link =~ ("^https?://(www.|m.)?imgur.com/.*$" :: String) :: [[String]] of
     [_] -> do
       putStrLn "Found other imgur link"
       let link' =
@@ -188,27 +205,32 @@ imgurOtherHandler link =
               [[_, baseUrl]] -> baseUrl
               _ -> link
       let imageLink1 =
-            trim <$>
-            readCreateProcess
-              (shell $
-               "curl -L -s '" ++
-               link' ++
-               "' | sed -n 's/.*contentURL.*content=\"\\(.*\\)\".*/\\1/p'")
-              ""
+            trim
+              <$> readCreateProcess
+                ( shell $
+                    "curl -L -s '"
+                      ++ link'
+                      ++ "' | sed -n 's/.*contentURL.*content=\"\\(.*\\)\".*/\\1/p'"
+                )
+                ""
       let imageLink2 =
-            trim <$>
-            readCreateProcess
-              (shell $
-               "curl -L -s '" ++
-               link' ++ "' | sed -n 's/.*image_src.*href=\"\\(.*\\)\".*/\\1/p'")
-              ""
+            trim
+              <$> readCreateProcess
+                ( shell $
+                    "curl -L -s '"
+                      ++ link'
+                      ++ "' | sed -n 's/.*image_src.*href=\"\\(.*\\)\".*/\\1/p'"
+                )
+                ""
       let imageLink3 =
-            trim <$>
-            readCreateProcess
-              (shell $
-               "curl -L -s '" ++
-               link' ++ "' | grep -wo 'https://i.imgur[^\"]*' | grep -v '?'")
-              ""
+            trim
+              <$> readCreateProcess
+                ( shell $
+                    "curl -L -s '"
+                      ++ link'
+                      ++ "' | grep -wo 'https://i.imgur[^\"]*' | grep -v '?'"
+                )
+                ""
       (: []) <$> (imageLink1 <|> imageLink2 <|> imageLink3)
     _ -> return []
   where
@@ -218,47 +240,35 @@ imgurOtherHandler link =
         "" -> b
         s -> return s
 
-gfycatHandler :: Handler
+gfycatHandler :: DirectLinkHandler
 gfycatHandler link =
-  case link =~
-       ("^https?://(www.|)gfycat.com(/..|/gifs/detail)?/([a-zA-Z]*)(-.*)?/?(\\?.*)?$" :: String) :: [[String]] of
-    [[_, _, _, gfycatId, _, _]] -> withGfycatId gfycatId
+  case link
+         =~ ("^https?://(www.|)gfycat.com(/..|/gifs/detail)?/([a-zA-Z]*)(-.*)?/?(\\?.*)?$" :: String) ::
+         [[String]] of
+    [[_, _, _, gfycatId, _, _]] -> return ["https://www.gfycat.com/" <> gfycatId]
     _ -> return []
 
-redgifHandler :: Handler
+redgifHandler :: DirectLinkHandler
 redgifHandler link =
-  case link =~ ("https?://(www.|)redgifs.com/watch/([a-zA-Z]*)" :: String) :: [[String]] of
-    [[_, _, gfycatId]] -> withGfycatId gfycatId
+  case link =~ ("^https?://(www.|)redgifs.com/(watch|ifr)/([a-zA-Z]*)$" :: String) :: [[String]] of
+    [[_, _, _, gfycatId]] -> return ["https://www.redgifs.com/watch/" <> gfycatId]
     _ -> return []
-  where
-    fallback :: String -> HttpException -> IO [String]
-    fallback gfycatId _ =
-      (\newId -> ["https://thumbs1.redgifs.com/" <> newId <> ".webm"]) . trim <$>
-      readCreateProcess
-        (shell $
-         "curl -L -s '" <>
-         link <> "' | sed -n 's#.*watch/\\([^\"]*\\).*#\\1#p' | head -n1")
-        ""
 
-withGfycatId :: String -> IO [String]
-withGfycatId gfycatId =
-  (do putStrLn $ "Gfycat ID: " ++ gfycatId
-      (\case
-         Nothing -> []
-         Just x -> [x]) .
-        fmap mp4Url . Aeson.decode . (^. Wreq.responseBody) <$>
-        Wreq.get ("https://api.gfycat.com/v1/gfycats/" ++ gfycatId)) `catch`
-  (\e ->
-     case e of
-       HttpExceptionRequest _ (StatusCodeException _ _) ->
-         withRedgifsId gfycatId
-       _ -> throw e)
+redditVideoHandler :: DirectLinkHandler
+redditVideoHandler link =
+  case link =~ ("^https://v.redd.it/([a-zA-Z0-9]*)$" :: String) :: [[String]] of
+    [[_, _]] -> return [link]
+    _ -> return []
 
-withRedgifsId :: String -> IO [String]
-withRedgifsId redgifsId = do
-  putStrLn $ "Redgifs ID: " ++ redgifsId
-  (\case
-     Nothing -> []
-     Just x -> [x]) .
-    fmap mp4Url . Aeson.decode . (^. Wreq.responseBody) <$>
-    Wreq.get ("https://api.redgifs.com/v1/gfycats/" ++ redgifsId)
+redditPreviewHandler :: DirectLinkHandler
+redditPreviewHandler link =
+  case link =~ ("^https://preview.redd.it/([a-zA-Z0-9]*)(.png|.jpg)(\\?.*)?$" :: String) :: [[String]] of
+    [[_, redditImageId, extension, _]] -> return ["https://i.redd.it/" <> redditImageId <> extension]
+    _ -> return []
+
+xvideosHandler :: DirectLinkHandler
+xvideosHandler link =
+  -- https://github.com/ytdl-org/youtube-dl/issues/30271
+  case link =~ ("^https://www.xvideos.com/([a-zA-Z0-9]*)/.*$" :: String) :: [[String]] of
+    [[_, _]] -> return [link]
+    _ -> return []
--- src/Model/DownloadManager.hs
+++ src/Model/DownloadManager.hs
@@ -1,20 +1,22 @@
 module Model.DownloadManager
-  ( initialize
-  ) where
+  ( initialize,
+  )
+where
 
 import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan (Chan, getChanContents, newChan, writeChan)
 import Control.Monad (forM_, replicateM_)
-import qualified Data.Text as Text
 import Data.Text (Text)
-import ImageLink (imageLinks)
-import Model.Types (Config(aria2Secret, path))
+import qualified Data.Text as Text
+import ImageLink (DownloadHandler (DirectLink, YoutubeDL), imageLinks)
+import Model.Types (Config (aria2Secret, path))
 import qualified Network.Aria2 as Aria2
 import qualified Reddit as R
 import qualified Reddit.Types.Post as R
 import System.Directory (createDirectoryIfMissing, doesFileExist, renameFile)
 import System.FilePath.Posix (takeExtension)
-import Types (Command(Download))
+import System.Process (readCreateProcess, readCreateProcessWithExitCode, shell)
+import Types (Command (Download))
 
 initialize :: Config -> Chan Command -> IO (Chan R.Post)
 initialize config modelChannel = do
@@ -22,68 +24,107 @@ initialize config modelChannel = do
   aria2OutputChan <- newChan
   _ <-
     forkIO
-      (Aria2.run
-         (Aria2.defaultOptions {Aria2.secret = aria2Secret config})
-         aria2InputChan
-         aria2OutputChan)
+      ( Aria2.run
+          (Aria2.defaultOptions {Aria2.secret = aria2Secret config})
+          aria2InputChan
+          aria2OutputChan
+      )
   _ <-
     forkIO $
-    getChanContents aria2OutputChan >>=
-    mapM_
-      (\((pid, index, count, fname), result) ->
-         case result of
-           Left msg ->
-             print msg >> writeChan modelChannel (Download pid index count [])
-           Right f' ->
-             renameFile (Text.unpack f') fname >>
-             writeChan modelChannel (Download pid index count [fname]))
+      getChanContents aria2OutputChan
+        >>= mapM_
+          ( \((pid, index, count, fname), result) ->
+              case result of
+                Left msg ->
+                  print msg >> writeChan modelChannel (Download pid index count [])
+                Right f' ->
+                  renameFile (Text.unpack f') fname
+                    >> writeChan modelChannel (Download pid index count [fname])
+          )
   downloadChannel <- newChan
   replicateM_
     6
-    (forkIO
-       (getChanContents downloadChannel >>=
-        mapM_ (downloadPost config modelChannel aria2InputChan)))
+    ( forkIO
+        ( getChanContents downloadChannel
+            >>= mapM_ (downloadPost config modelChannel aria2InputChan)
+        )
+    )
   return downloadChannel
 
 downloadPost ::
-     Config
-  -> Chan Command
-  -> Chan ((R.PostID, Int, Int, FilePath), Text)
-  -> R.Post
-  -> IO ()
-downloadPost config channel aria2InputChan p@R.Post { R.content = R.Link txt
-                                                    , R.postID = R.PostID pid
-                                                    } = do
-  let link = Text.unpack txt
-  dl <- imageLinks link
-  case dl of
-    _:_ -> do
-      let dname =
-            Text.unpack $
-            path config <>
-            (let R.R subr = R.subreddit p
-              in subr)
-      createDirectoryIfMissing False dname
-      forM_ (zip [1 ..] dl) $ \(i, dl') -> do
-        let fname =
-              if length dl == 1
-                then dname <> "/" <> Text.unpack pid <> takeExtension dl'
-                else dname <>
-                     "/" <>
-                     Text.unpack pid <> "-" <> show i <> takeExtension dl'
-        doesFileExist fname >>= \case
-          True -> do
-            putStrLn $
-              "File exists " <> link <> " (" <> dl' <> ") into " <> fname
-            writeChan channel (Download (R.postID p) i (length dl) [fname])
-          False -> do
-            putStrLn $
-              "Downloading " <> link <> " (" <> dl' <> ") into " <> fname
-            writeChan
-              aria2InputChan
-              ((R.postID p, i, length dl, fname), Text.pack dl')
-    _ -> do
-      putStrLn $ "Unable to download " ++ link ++ " " ++ show dl ++ " sub=" ++ (let R.R subr = R.subreddit p in show subr)
-      writeChan channel (Download (R.postID p) 1 1 [])
+  Config ->
+  Chan Command ->
+  Chan ((R.PostID, Int, Int, FilePath), Text) ->
+  R.Post ->
+  IO ()
+downloadPost
+  config
+  channel
+  aria2InputChan
+  p@R.Post
+    { R.content = R.Link txt,
+      R.postID = R.PostID pid
+    } = do
+    let link = Text.unpack txt
+    dl <- imageLinks link
+    case dl of
+      _ : _ -> do
+        let dname =
+              Text.unpack $
+                path config
+                  <> ( let R.R subr = R.subreddit p
+                        in subr
+                     )
+        createDirectoryIfMissing False dname
+        forM_ (zip [1 ..] dl) $ \(i, dl') -> do
+          let extension = case snd dl' of
+                YoutubeDL -> ".mp4"
+                DirectLink -> takeExtensionOnly (fst dl')
+          let fname =
+                if length dl == 1
+                  then dname <> "/" <> Text.unpack pid <> extension
+                  else
+                    dname
+                      <> "/"
+                      <> Text.unpack pid
+                      <> "-"
+                      <> show i
+                      <> extension
+          doesFileExist fname >>= \case
+            True -> do
+              putStrLn $
+                "File exists " <> link <> " (" <> show dl' <> ") into " <> fname
+              writeChan channel (Download (R.postID p) i (length dl) [fname])
+            False -> do
+              putStrLn $
+                "Downloading " <> link <> " (" <> show dl' <> ") into " <> fname
+              case snd dl' of
+                YoutubeDL ->
+                  readCreateProcessWithExitCode
+                    (shell $ "youtube-dl -w -o '" <> fname <> "' --newline '" <> fst dl' <> "'")
+                    ""
+                    >>= ( \(exitCode, stdout, stderr) ->
+                            putStrLn (show dl' <> " failed with code: " <> show exitCode)
+                              <> putStrLn stdout
+                              <> putStrLn ""
+                              <> putStrLn stderr
+                        )
+                    >> doesFileExist fname
+                    >>= \case
+                      False -> writeChan channel (Download (R.postID p) i (length dl) [])
+                      True ->
+                        writeChan
+                          channel
+                          (Download (R.postID p) i (length dl) [fname])
+                DirectLink ->
+                  writeChan
+                    aria2InputChan
+                    ((R.postID p, i, length dl, fname), Text.pack (fst dl'))
+      _ -> do
+        putStrLn $ "Unable to download " ++ link ++ " " ++ show dl ++ " sub=" ++ (let R.R subr = R.subreddit p in show subr)
+        writeChan channel (Download (R.postID p) 1 1 [])
 downloadPost _ channel _ R.Post {R.postID = i} =
   writeChan channel (Download i 1 1 [])
+
+takeExtensionOnly :: String -> String
+takeExtensionOnly = takeWhile (/= '?') . takeExtension
